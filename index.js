dd42c60826ce3e10cf0689bdfaf662fd3cceeeb4
John <jpena91@gmail.com>
2019-05-02 09:20:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+      return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  //not my code but "copy and paste is life in programming" - Good Programmer.
+
+  static titleize(string) {
+  let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+  let result = [];
+  let arrayOfWords = string.split(" ")
+  for (let n = 0; n < arrayOfWords.length; n++) {
+    if (n == 0) {
+      result.push(this.capitalize(arrayOfWords[n]))
+    } else {
+      if (exceptions.includes(arrayOfWords[n])) {
+        result.push(arrayOfWords[n])
+      } else {
+        result.push(this.capitalize(arrayOfWords[n]))
+      }
+    }
+
+  }
+  return result.join(" ");
+}
+
 }
